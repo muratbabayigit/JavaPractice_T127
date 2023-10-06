fca5b6daf7ed4bd18f1d9bd7f4d66cbec6c565e4
Murat Babayiğit <babayigit@gmail.com>
2023-10-06 22:07:18 +0300

    2023106

--- src/day05/P05.java
+++ src/day05/P05.java
@@ -1,10 +1,26 @@
 package day05;
 
+import java.util.Scanner;
+
 public class P05 {
     public static void main(String[] args) {
         // Kullanıcıdan 5 adet sayı isteyiniz
         // Bu sayılardan 5 ile 10 arasındakiler hariç, diğerlerinin toplamını bulunuz.
 
+        Scanner scan=new Scanner(System.in);
+
+        int sayi=0;
+        int toplam=0;
+
+        for (int i = 0; i <5 ; i++) {
+            System.out.print(i+1 +". sayıyı giriniz: ");
+            sayi= scan.nextInt();
+          if(sayi>=5 && sayi<=10){
+              continue;
+          }
+          toplam+=sayi;
+        }
+        System.out.println("toplam = " + toplam);
 
     }
 }
