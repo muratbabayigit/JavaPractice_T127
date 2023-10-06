c6dc74d9603930a8c5799bafa5d10853884c67c8
Murat Babayiğit <babayigit@gmail.com>
2023-10-06 11:08:14 +0300

    20231006

---
+++ src/day05/P05.java
@@ -0,0 +1,10 @@
+package day05;
+
+public class P05 {
+    public static void main(String[] args) {
+        // Kullanıcıdan 5 adet sayı isteyiniz
+        // Bu sayılardan 5 ile 10 arasındakiler hariç, diğerlerinin toplamını bulunuz.
+
+
+    }
+}
